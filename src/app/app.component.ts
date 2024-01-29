d9d859e9a1460d48c29a2bd94f5d0bedde259dab
Daniil Lytvyn <daniilly@softwareplanetgroup.com>
2024-01-29 21:16:58 +0200

    Add drop-down menu navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,102 +2,109 @@ import {
     AfterViewInit,
     Component,
     ElementRef,
+    Inject,
     OnDestroy,
+    OnInit,
+    QueryList,
     ViewChild,
+    ViewChildren,
 } from "@angular/core";
+import { DOCUMENT } from "@angular/common";
 import {
-    distinctUntilChanged,
+    BehaviorSubject,
     debounceTime,
-    ReplaySubject,
-    Subject,
+    delay,
+    distinctUntilChanged,
+    filter,
     fromEvent,
-    takeUntil,
-    shareReplay,
-    switchMap,
     map,
     merge,
     Observable,
-    tap,
-    filter,
-    delay,
     of,
+    pipe,
+    ReplaySubject,
+    shareReplay,
+    Subject,
+    switchMap,
+    takeUntil,
+    tap,
+    withLatestFrom,
 } from "rxjs";
 import { ProclaimedStatus, withStatusProclaim } from "./shared/operators";
+
+enum ArrowKeyDirection {
+    Up = -1,
+    Down = 1,
+}
+
+/**
+ * Calculates the correct modulo operation in JavaScript, handling negative numbers.
+ *
+ * JavaScript's native modulo operator (%) may not behave as expected for negative numbers.
+ * This function ensures correct modulo calculation for both positive and negative numbers.
+ *
+ * @param {number} n - The dividend.
+ * @param {number} m - The divisor.
+ * @returns {number} The result of the modulo operation (n % m).
+ */
+function mod(n: number, m: number): number {
+    return ((n % m) + m) % m;
+}
 
 @Component({
     selector: "app-root",
     templateUrl: "./app.component.html",
     styleUrls: ["./app.component.scss"],
 })
-export class AppComponent implements AfterViewInit, OnDestroy {
+export class AppComponent implements AfterViewInit, OnDestroy, OnInit {
+    // Begin counting from -1 instead of 0 to ensure that
+    // we end up at index 0 by first forward navigation.
+    readonly startListIndex: number = -1;
+
     @ViewChild("inputBox", { static: true })
     inputRef: ElementRef | undefined;
 
     @ViewChild("clearBtn", { static: true })
     clearBtnRef: ElementRef | undefined;
 
-    inputChanges$: Subject<KeyboardEvent> = new Subject<KeyboardEvent>();
-    clearButtonClick$: Subject<MouseEvent> = new Subject<MouseEvent>();
-    private destroy$: ReplaySubject<null> = new ReplaySubject<null>();
+    @ViewChildren("listItem")
+    resultListItems: QueryList<ElementRef> | undefined;
 
-    clearInput$: Observable<MouseEvent> = this.clearButtonClick$.pipe(
-        tap((_) => this.setInputValue(""))
+    inputChange$: Subject<KeyboardEvent | ClipboardEvent> = new Subject();
+    hostKeydown$: Subject<KeyboardEvent> = new Subject();
+    clearButtonClick$: Subject<MouseEvent> = new Subject();
+
+    activeListItemIndex$: BehaviorSubject<number> = new BehaviorSubject(
+        this.startListIndex
     );
+    private destroy$: ReplaySubject<null> = new ReplaySubject();
 
-    backspaceKeyup$: Observable<KeyboardEvent> = this.inputChanges$.pipe(
-        filter((event: KeyboardEvent) => event.key === "Backspace")
-    );
+    constructor(@Inject(DOCUMENT) private document: Document) {}
 
-    inputValueTrimmed$: Observable<string> = this.inputChanges$.pipe(
-        map((event: KeyboardEvent) =>
-            (event.target as HTMLInputElement).value.trim()
-        ),
-        shareReplay({
-            bufferSize: 1,
-            refCount: false,
-        })
-    );
+    nextListIndexToSelect = ([direction, currentSelectedIndex]: [
+        number,
+        number,
+    ]): number => {
+        const listItems: ElementRef[] | undefined =
+            this.resultListItems?.toArray();
+        const listLength: number = listItems ? listItems.length : 0;
 
-    searchStatus$: Observable<ProclaimedStatus<Array<string>>> =
-        this.inputValueTrimmed$.pipe(
-            debounceTime(200),
-            distinctUntilChanged(),
-            filter(Boolean),
-            switchMap((query: string) =>
-                of([]).pipe(delay(1000), withStatusProclaim)
-            ),
-            shareReplay({
-                bufferSize: 1,
-                refCount: false,
-            })
-        );
+        if (direction === ArrowKeyDirection.Up) {
+            return currentSelectedIndex <= 0
+                ? listLength + direction
+                : currentSelectedIndex + direction;
+        }
 
-    suggestions$: Observable<Array<string>> = this.searchStatus$.pipe(
-        map(
-            (statusReport: ProclaimedStatus<Array<string>>): Array<string> =>
-                statusReport.status === "resolved" ? statusReport.value : []
-        )
-    );
+        return mod(currentSelectedIndex + direction, listLength);
+    };
 
-    refreshSuggestions$: Observable<Array<string>> = merge(
-        this.clearInput$,
-        this.backspaceKeyup$
-    ).pipe(map(() => []));
+    setFocusOnListItem = (index: number): void => {
+        const listItems: ElementRef[] | undefined =
+            this.resultListItems?.toArray();
 
-    displayResults$: Observable<Array<string>> = merge(
-        this.suggestions$,
-        this.refreshSuggestions$
-    );
-
-    shouldDisplayNoResults = (
-        status: ProclaimedStatus<Array<unknown>> | null,
-        inputValue: string | null
-    ): boolean => {
-        const isResolved = status?.status === "resolved";
-        const inputIsEmpty = (inputValue?.length ?? 0) === 0;
-        const resultsLength = isResolved ? status.value.length : 0;
-
-        return isResolved && !inputIsEmpty && resultsLength === 0;
+        if (listItems?.[index]) {
+            listItems[index].nativeElement.focus();
+        }
     };
 
     setInputValue = (newValue: string): void => {
@@ -112,6 +119,148 @@ export class AppComponent implements AfterViewInit, OnDestroy {
         inputElement.dispatchEvent(new Event("paste"));
     };
 
+    redirectBySelectedItem = (index: number, listItems: string[]): void => {
+        const wantedQueryString = listItems[index];
+
+        if (!wantedQueryString) {
+            console.debug(`Could not get required query. The index is ${index}; 
+            results: ${listItems}`);
+            return;
+        }
+
+        const start = 96; // "a".charCodeAt(0) - 1
+        const len = wantedQueryString.length;
+        const out = [...wantedQueryString.toLowerCase()].reduce(
+            (out, char, pos) => {
+                const val = char.charCodeAt(0) - start;
+                const pow = Math.pow(26, len - pos - 1);
+                return out + val * pow;
+            },
+            0
+        );
+
+        this.document.location.href =
+            "https://stackoverflow.com/questions/" + out;
+    };
+
+    getInputValueFromEvent = (event: KeyboardEvent | ClipboardEvent) => {
+        // If the event originates from the paste event, we cannot
+        // retrieve the pasted text directly from the input element
+        // or event itself. Therefore, we use the Clipboard API.
+        if (event.type === "paste") {
+            const clipboardData = (event as ClipboardEvent).clipboardData;
+            return clipboardData?.getData("text/plain")?.trim() ?? "";
+        }
+
+        return (event.target as HTMLInputElement).value.trim();
+    };
+
+    shouldDisplayNoResults = (
+        status: ProclaimedStatus<Array<unknown>> | null,
+        inputValue: string | null
+    ): boolean => {
+        const isResolved = status?.status === "resolved";
+        const inputIsEmpty = (inputValue?.length ?? 0) === 0;
+        const resultsLength = isResolved ? status.value.length : 0;
+
+        return isResolved && !inputIsEmpty && resultsLength === 0;
+    };
+
+    isUserFocusedOnListItem = (): boolean => {
+        return this.document.activeElement?.tagName === "LI";
+    };
+
+    clearInput$: Observable<MouseEvent> = this.clearButtonClick$.pipe(
+        tap((_) => this.setInputValue(""))
+    );
+
+    documentEnter$: Observable<KeyboardEvent> = this.hostKeydown$.pipe(
+        filter((event: KeyboardEvent) => event.key === "Enter")
+    );
+
+    documentArrowUp$: Observable<number> = this.hostKeydown$.pipe(
+        filter((event: KeyboardEvent) => event.key === "ArrowUp"),
+        map((_) => ArrowKeyDirection.Up)
+    );
+
+    documentArrowDown$: Observable<number> = this.hostKeydown$.pipe(
+        filter((event: KeyboardEvent) => event.key === "ArrowDown"),
+        map((_) => ArrowKeyDirection.Down)
+    );
+
+    inputBackspace$: Observable<KeyboardEvent> = this.inputChange$.pipe(
+        filter<any>(
+            (event) =>
+                event.type === "keyup" &&
+                (event as KeyboardEvent)?.key === "Backspace"
+        )
+    );
+
+    inputValueTrimmed$: Observable<string> = this.inputChange$.pipe(
+        map(this.getInputValueFromEvent),
+        shareReplay({
+            bufferSize: 1,
+            refCount: false,
+        })
+    );
+
+    searchResponse$: Observable<ProclaimedStatus<Array<string>>> =
+        this.inputValueTrimmed$.pipe(
+            debounceTime(300),
+            distinctUntilChanged(),
+            filter(Boolean),
+            switchMap((query: string) =>
+                of(["one", "two", "three"]).pipe(delay(200), withStatusProclaim)
+            ),
+            shareReplay({
+                bufferSize: 1,
+                refCount: false,
+            })
+        );
+
+    resetSearchResult$: Observable<Array<string>> = merge(
+        this.clearInput$,
+        this.inputBackspace$
+    ).pipe(map(() => []));
+
+    searchResult$: Observable<Array<string>> = merge(
+        this.resetSearchResult$,
+        this.searchResponse$.pipe(
+            map(
+                (
+                    statusReport: ProclaimedStatus<Array<string>>
+                ): Array<string> =>
+                    statusReport.status === "resolved" ? statusReport.value : []
+            )
+        )
+    ).pipe(tap((_) => this.activeListItemIndex$.next(this.startListIndex)));
+
+    listNavigation$: Observable<unknown> = merge(
+        this.documentArrowDown$,
+        this.documentArrowUp$
+    ).pipe(
+        withLatestFrom(this.activeListItemIndex$),
+        map(this.nextListIndexToSelect),
+        tap(this.setFocusOnListItem),
+        tap((newIndex) => this.activeListItemIndex$.next(newIndex))
+    );
+
+    setInputFocus$: Observable<unknown> = this.hostKeydown$.pipe(
+        filter(this.isUserFocusedOnListItem),
+        filter(
+            (event) => !["ArrowDown", "ArrowUp", "Enter"].includes(event.key)
+        ),
+        tap(() => this.inputRef?.nativeElement?.focus())
+    );
+
+    handleUserChoice$: Observable<unknown> = this.documentEnter$.pipe(
+        filter(this.isUserFocusedOnListItem),
+        withLatestFrom(this.activeListItemIndex$, this.searchResult$),
+        tap(([_, index, result]) => {
+            this.redirectBySelectedItem(index, result);
+        })
+    );
+
     ngAfterViewInit(): void {
         if (this.inputRef !== undefined) {
             merge(
@@ -119,7 +268,7 @@ export class AppComponent implements AfterViewInit, OnDestroy {
                 fromEvent<KeyboardEvent>(this.inputRef.nativeElement, "paste")
             )
                 .pipe(takeUntil(this.destroy$))
-                .subscribe(this.inputChanges$);
+                .subscribe(this.inputChange$);
         }
 
         if (this.clearBtnRef !== undefined) {
@@ -127,6 +276,20 @@ export class AppComponent implements AfterViewInit, OnDestroy {
                 .pipe(takeUntil(this.destroy$))
                 .subscribe(this.clearButtonClick$);
         }
+
+        fromEvent<KeyboardEvent>(this.document, "keydown")
+            .pipe(takeUntil(this.destroy$))
+            .subscribe(this.hostKeydown$);
+    }
+
+    ngOnInit() {
+        const terminationPipe = pipe(takeUntil(this.destroy$));
+
+        [
+            this.listNavigation$,
+            this.setInputFocus$,
+            this.handleUserChoice$,
+        ].forEach((o) => o.pipe(terminationPipe).subscribe());
     }
 
     ngOnDestroy(): void {
